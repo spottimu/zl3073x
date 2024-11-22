14d66b764a2e81d87052ed6a239f7f78b3833d74
Ivan Vecera <ivecera@redhat.com>
2024-11-22 18:16:44 +0100

    mfd: zl80x32: Add dummy devlink flash callback
    Provide a dummy devlink flash callback, the full functionality is added
    by subsequent patches from this series.
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/mfd/zl3073x.h
+++ drivers/mfd/zl3073x.h
@@ -4,10 +4,18 @@
 #define __ZL3073X_CORE_H
 
 #include <linux/mfd/zl3073x.h>
+#include <net/devlink.h>
 
 struct zl3073x_dev *zl3073x_dev_alloc(struct device *dev);
 int zl3073x_dev_init(struct zl3073x_dev *zldev, u8 dev_id);
 void zl3073x_dev_exit(struct zl3073x_dev *zldev);
 const struct regmap_config *zl3073x_get_regmap_config(void);
+
+/*
+ * Misc functions
+ */
+int zl3073x_flash_update(struct devlink *devlink,
+			 struct devlink_flash_update_params *params,
+			 struct netlink_ext_ack *extack);
 
 #endif /* __ZL3073X_CORE_H */
