9b7045ce25db05eb10f4a9b6bb8b116030d04c09
Ivan Vecera <ivecera@redhat.com>
2025-01-29 14:25:39 +0100

    mfd: zl3073x: Register DPLL sub-device during init
    Register DPLL sub-devices to expose the functionality provided
    by ZL3073x chip family. Each sub-device represents one of
    the available DPLL channels.
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    ---
    v7->v8:
    * replaced zl3073x_pdata array ZL3073X_PDATA macro
    v6->v7:
    * use platform data to pass the channel to use
    v4->v6:
    * no change
    v3->v4:
    * use static mfd cells

--- drivers/mfd/zl3073x-core.c
+++ drivers/mfd/zl3073x-core.c
@@ -7,6 +7,7 @@
 #include <linux/device.h>
 #include <linux/export.h>
 #include <linux/math64.h>
+#include <linux/mfd/core.h>
 #include <linux/mfd/zl3073x.h>
 #include <linux/module.h>
 #include <linux/netlink.h>
@@ -752,6 +753,23 @@ static void zl3073x_devlink_unregister(void *ptr)
 	devlink_unregister(ptr);
 }
 
+#define ZL3073X_PDATA(_channel)			\
+	(&(const struct zl3073x_pdata) {	\
+		.channel = _channel,		\
+	})
+
+#define ZL3073X_CELL(_name, _channel)				\
+	MFD_CELL_BASIC(_name, NULL, ZL3073X_PDATA(_channel),	\
+		       sizeof(struct zl3073x_pdata), 0)
+
+static const struct mfd_cell zl3073x_devs[] = {
+	ZL3073X_CELL("zl3073x-dpll", 0),
+	ZL3073X_CELL("zl3073x-dpll", 1),
+	ZL3073X_CELL("zl3073x-dpll", 2),
+	ZL3073X_CELL("zl3073x-dpll", 3),
+	ZL3073X_CELL("zl3073x-dpll", 4),
+};
+
 /**
  * zl3073x_dev_probe - initialize zl3073x device
  * @zldev: pointer to zl3073x device
@@ -822,6 +840,15 @@ int zl3073x_dev_probe(struct zl3073x_dev *zldev,
 	rc = zl3073x_dev_state_fetch(zldev);
 	if (rc)
 		return rc;
+
+	/* Add DPLL sub-device cell for each DPLL channel */
+	rc = devm_mfd_add_devices(zldev->dev, PLATFORM_DEVID_AUTO, zl3073x_devs,
+				  chip_info->num_channels, NULL, 0, NULL);
+	if (rc) {
+		dev_err_probe(zldev->dev, rc,
+			      "Failed to add DPLL sub-device\n");
+		return rc;
+	}
 
 	/* Register the device as devlink device */
 	devlink = priv_to_devlink(zldev);
