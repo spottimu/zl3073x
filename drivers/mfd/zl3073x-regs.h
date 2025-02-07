4878a187c5464fb04e92cd0e42fb725110feabfd
Ivan Vecera <ivecera@redhat.com>
2025-02-07 15:49:35 +0100

    mfd: zl3073x: Fetch invariants during probe
    Several configuration parameters will remain constant at runtime,
    so we can load them during probe to avoid excessive reads from
    the hardware.
    
    These parameters will be frequently accessed by the DPLL sub-device
    driver (in follow-up series), and later by the PHC/PTP sub-device
    driver.
    
    Read the following parameters from the device during probe and store
    them for later use:
    
    * frequencies of the synthesizers and their associated DPLL channels
    * enablement and type (single-ended or differential) of input pins
    * associated synthesizers, signal format, and enablement status of
      outputs
    
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>
    ---
    v8->v9:
    * provide synth enablement info
    v7->v8:
    * no change
    v6->v7:
    * export zl3073x_mb_op() - will be used immediatelly in part2
    v5->v6:
    * no change
    v4->v5:
    * updated to use new register API
    v3->v4:
    * adjusted for new mailbox API
    v2->v3:
    * dropped usage of macros for generating helper functions
    v1->v2:
    * fixed and added inline documentation

--- drivers/mfd/zl3073x-regs.h
+++ drivers/mfd/zl3073x-regs.h
@@ -14,4 +14,41 @@
 #define ZL_REG_FW_VER				ZL_REG(0, 0x05, 2)
 #define ZL_REG_CUSTOM_CONFIG_VER		ZL_REG(0, 0x07, 4)
 
+/***********************************
+ * Register Page 9, Synth and Output
+ ***********************************/
+
+#define ZL_REG_SYNTH_CTRL(_idx)						\
+	ZL_REG_IDX(_idx, 9, 0x00, 1, ZL3073X_NUM_SYNTHS, 1)
+#define ZL_SYNTH_CTRL_EN			BIT(0)
+#define ZL_SYNTH_CTRL_DPLL_SEL			GENMASK(6, 4)
+
+#define ZL_REG_OUTPUT_CTRL(_idx)					\
+	ZL_REG_IDX(_idx, 9, 0x28, 1, ZL3073X_NUM_OUTPUTS, 1)
+#define ZL_OUTPUT_CTRL_EN			BIT(0)
+#define ZL_OUTPUT_CTRL_SYNTH_SEL		GENMASK(6, 4)
+
+/*******************************
+ * Register Page 10, Ref Mailbox
+ *******************************/
+
+#define ZL_REG_REF_CONFIG			ZL_REG(10, 0x0d, 1)
+#define ZL_REF_CONFIG_ENABLE			BIT(0)
+#define ZL_REF_CONFIG_DIFF_EN			BIT(2)
+
+/*********************************
+ * Register Page 13, Synth Mailbox
+ *********************************/
+
+#define ZL_REG_SYNTH_MB_MASK			ZL_REG(13, 0x02, 2)
+
+#define ZL_REG_SYNTH_MB_SEM			ZL_REG(13, 0x04, 1)
+#define ZL_SYNTH_MB_SEM_WR			BIT(0)
+#define ZL_SYNTH_MB_SEM_RD			BIT(1)
+
+#define ZL_REG_SYNTH_FREQ_BASE			ZL_REG(13, 0x06, 2)
+#define ZL_REG_SYNTH_FREQ_MULT			ZL_REG(13, 0x08, 4)
+#define ZL_REG_SYNTH_FREQ_M			ZL_REG(13, 0x0c, 2)
+#define ZL_REG_SYNTH_FREQ_N			ZL_REG(13, 0x0e, 2)
+
 #endif /* __ZL3073X_REGS_H */
