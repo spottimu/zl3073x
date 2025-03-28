64f149d1c99ae576531fc679dfbdad7ffe8ddd91
Ivan Vecera <ivecera@redhat.com>
2025-03-28 14:56:53 +0100

    dpll: zl3073x: Add support to get phase offset on input pins
    Add support to get phase offset for the input pins. Implement the appropriate
    callback that performs DPLL to reference phase error measurement and reports
    the measured value. If the DPLL is currently locked to different reference
    with higher frequency then the phase offset is modded to the period of
    the signal the DPLL is locked to.
    
    Co-developed-by: Prathosh Satish <Prathosh.Satish@microchip.com>
    Signed-off-by: Prathosh Satish <Prathosh.Satish@microchip.com>
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- include/linux/mfd/zl3073x-regs.h
+++ include/linux/mfd/zl3073x-regs.h
@@ -84,6 +84,16 @@
 #define ZL_DPLL_REFSEL_STATUS_STATE		GENMASK(6, 4)
 #define ZL_DPLL_REFSEL_STATUS_STATE_LOCK	4
 
+/**********************
+ * Register Page 4, Ref
+ **********************/
+
+#define ZL_REG_REF_PHASE_ERR_READ_RQST		ZL_REG(4, 0x0f, 1)
+#define ZL_REF_PHASE_ERR_READ_RQST_RD		BIT(0)
+
+#define ZL_REG_REF_PHASE(_idx)						\
+	ZL_REG_IDX(_idx, 4, 0x20, 6, ZL3073X_NUM_INPUTS, 6)
+
 /***********************
  * Register Page 5, DPLL
  ***********************/
@@ -97,6 +107,12 @@
 #define ZL_DPLL_MODE_REFSEL_MODE_AUTO		3
 #define ZL_DPLL_MODE_REFSEL_MODE_NCO		4
 #define ZL_DPLL_MODE_REFSEL_REF			GENMASK(7, 4)
+
+#define ZL_REG_DPLL_MEAS_CTRL			ZL_REG(5, 0x50, 1)
+#define ZL_DPLL_MEAS_CTRL_EN			BIT(0)
+
+#define ZL_REG_DPLL_MEAS_IDX			ZL_REG(5, 0x51, 1)
+#define ZL_DPLL_MEAS_IDX			GENMASK(2, 0)
 
 /***********************************
  * Register Page 9, Synth and Output
